cc9c47cc67685b48e0168fa3db7a9657baa0d4a3
gandersson <gabriel.andersson@recordedfuture.com>
2023-11-11 15:59:29 +0100

    Added direct link to all pages at the bottom of spa navbar?

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { Button } from './ui/Button';
-import { GitHubIconLink } from './ui/GitHubIconLink';
 import { globalCss, styled } from './stitches.config';
 import { Home } from './components/Home';
 import { ExampleComponent } from './components/ExampleComponent';
@@ -13,10 +12,10 @@ import { Plats } from './components/info/Plats';
 import { Talare } from './components/Talare';
 import { Vigsel } from './components/Vigsel';
 import { ExampleTwoDeepComponent } from './components/ExampleTwoDeepComponent';
-import { WhereComponent } from './components/WhereComponent';
 import { SitemapLinkGenerator } from './components/SitemapLinkGenerator';
 import { PageNotFound } from './components/PageNotFound';
 import { Breadcrumbs } from './components/Breadcrumbs';
+import { RouterLink } from './ui/RouterLink';
 
 const AppContainer = styled('div', {
   maxWidth: '540px',
@@ -46,6 +45,44 @@ const BreadcrumbsNav = styled('nav', {
   margin: '18px 0',
 });
 
+const MainPage = styled('div', {
+  minHeight: '800px',
+});
+
+const Footer: React.VFC = () => (
+  <div>
+    <h3>Platskarta</h3>
+    <ul>
+      <li>
+        <RouterLink to="/">Hem</RouterLink>{' '}
+      </li>
+      <li>
+        <RouterLink to="/vigsel">Vigsel</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/osa">OSA</RouterLink>
+      </li>
+      <li>
+        <RouterLink to="/info">Info</RouterLink>
+        <ul>
+          <li>
+            <RouterLink to="/info/boende">Boende</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/info/plats">Plats</RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/info/klader">Kläder</RouterLink>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <RouterLink to="/talare">Tal & Spex</RouterLink>
+      </li>
+    </ul>
+  </div>
+);
+
 export const App: React.VFC = () => {
   globalCss();
 
@@ -53,33 +90,36 @@ export const App: React.VFC = () => {
     <AppContainer>
       <HeaderContainer>
         <H1>Carolina & Gabriel 2024</H1>
+        <RouterLink to="/vigsel">Vigsel</RouterLink>
       </HeaderContainer>
 
       <BreadcrumbsNav>
         <Breadcrumbs />
       </BreadcrumbsNav>
-
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/vigsel" component={Vigsel} />
-        <Route exact path="/osa" component={Osa} />
-        <Route exact path="/talare" component={Talare} />
-        <Route exact path="/info" component={Info} />
-        <Route exact path="/info/plats" component={Plats} />
-        <Route exact path="/info/kladsel" component={Clothing} />
-        <Route exact path="/info/boende" component={Boende} />
-        <Route
-          exact
-          path="/example/two-deep"
-          component={ExampleTwoDeepComponent}
-        />
-        <Route
-          exact
-          path="/sitemap-link-generator"
-          component={SitemapLinkGenerator}
-        />
-        <Route component={PageNotFound} />
-      </Switch>
+      <MainPage>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/vigsel" component={Vigsel} />
+          <Route exact path="/osa" component={Osa} />
+          <Route exact path="/talare" component={Talare} />
+          <Route exact path="/info" component={Info} />
+          <Route exact path="/info/plats" component={Plats} />
+          <Route exact path="/info/kladsel" component={Clothing} />
+          <Route exact path="/info/boende" component={Boende} />
+          <Route
+            exact
+            path="/example/two-deep"
+            component={ExampleTwoDeepComponent}
+          />
+          <Route
+            exact
+            path="/sitemap-link-generator"
+            component={SitemapLinkGenerator}
+          />
+          <Route component={PageNotFound} />
+        </Switch>
+      </MainPage>
+      <Footer />
     </AppContainer>
   );
 };
